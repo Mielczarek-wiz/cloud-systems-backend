7a7075cdc0409810fb67461d0862fe8ecfec1d6b
Jey the Count <58235578+JakubOwadowski@users.noreply.github.com>
2024-03-03 12:41:21 +0100

    removed duplicated cors

--- src/index.ts
+++ src/index.ts
@@ -18,13 +18,6 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
-  next();
-});
-
 app.get("/object", (req: Request, res: Response) => {
   getAll()
     .then((data) => {
