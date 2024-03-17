a875c263367e8382182df384bde3ecabe99d609a
Damian Mielczarek <mielczarek.damian99@o2.pl>
2024-03-17 21:29:53 +0100

    Change for Vagrant

--- src/index.ts
+++ src/index.ts
@@ -21,7 +21,7 @@ const port: number = Number(process.env.PORT) || 8080;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Origin", "http://192.168.50.4:3000");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type");
   next();
