dd4353545d39bd0d87a470b898eff906fd7cf86c
Damian Mielczarek <mielczarek.damian99@o2.pl>
2024-03-18 13:59:15 +0100

    Changed response of getRegions() error

--- src/index.ts
+++ src/index.ts
@@ -21,7 +21,7 @@ const port: number = Number(process.env.PORT) || 8080;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://192.168.50.4:3000");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type");
   next();
@@ -124,7 +124,7 @@ app.get("/region", async (req: Request, res: Response) => {
     .then((data) => {
       res.send(data);
     })
-    .catch((e: Error) => console.error(e));
+    .catch((e: Error) => res.send(e));
 });
 
 app.listen(port, () => {
