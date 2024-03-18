6c15d8478188636e26b7628d81824bc416490de5
Damian Mielczarek <mielczarek.damian99@o2.pl>
2024-03-18 19:55:33 +0100

    Cors fixing

--- src/index.ts
+++ src/index.ts
@@ -27,7 +27,7 @@ app.use(bodyParser.json());
 app.use((req, res, next) => {
   const origin = req.headers.origin as string;
   if (allowedOrigins.includes(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
+    res.header("Access-Control-Allow-Origin", origin);
   }
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type");
