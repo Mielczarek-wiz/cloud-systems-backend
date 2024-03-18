00d93b67a6c0da0ffc7dfb1825c7661aced76c33
Damian Mielczarek <mielczarek.damian99@o2.pl>
2024-03-18 18:54:07 +0100

    Last change for Vagrant and Ansible

--- src/index.ts
+++ src/index.ts
@@ -21,7 +21,10 @@ const port: number = Number(process.env.PORT) || 8080;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Origin",
+    `${process.env.NEXT_PUBLIC_API_URL}, ${process.env.NEXT_PRIVATE_API_URL}`
+  );
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type");
   next();
